49b90f6a84402e2f052c38012f92bcf821077f02
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2025-07-05 22:07:28 +0700

    memisahkan code content homepage ke file yg berbeda

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
+import { loadHomepage } from "./homepage";
+
 const content = document.getElementById("content");
 
-const welcomeHeading = document.createElement("h1");
-welcomeHeading.innerText = "Selamat datang di Lapo Batak Bahari";
-
-const introParagraph = document.createElement("p");
-introParagraph.innerText =
-  "Di sini, Anda bisa menikmati masakan Batak yang lezat sambil mendengar lagu 'Mardua Holong'.";
-
-const quotesParagraph = document.createElement("p");
-quotesParagraph.innerHTML =
-  "Jangan lupa makan <em>daging biang</em> dengan <em>sayur kol</em>";
-
-const lyricsHeading = document.createElement("h3");
-lyricsHeading.innerText = "Penggalan Lirik Mardua Holong";
-
-const lyricsDiv = document.createElement("div");
-lyricsDiv.innerHTML = `
-        <span>Tarsongon bunga naung malos di ladang i</span>
-        <br />
-        <span>Songoni ma rohakki nunga malala</span>
-        <br />
-        <span>Dang hurimpu songoni</span>
-        <br />
-        <span>Dibahen ho holonghi gabe meam-meammu</span>
-`;
-
-content.appendChild(welcomeHeading);
-content.appendChild(introParagraph);
-content.appendChild(quotesParagraph);
-content.appendChild(lyricsHeading);
-content.appendChild(lyricsDiv);
+content.appendChild(loadHomepage());
