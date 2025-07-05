e0db8a24597a609a6cfaaf0efd10bec1b48fbf85
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2025-07-05 20:47:59 +0700

    create homepage using createElement and appendChild

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("testing hot reload, sudah bisa connect dan auto reload");
+const content = document.getElementById("content");
+
+const welcomeHeading = document.createElement("h1");
+welcomeHeading.innerText = "Selamat datang di Lapo Batak Bahari";
+
+const introParagraph = document.createElement("p");
+introParagraph.innerText =
+  "Di sini, Anda bisa menikmati masakan Batak yang lezat sambil mendengar lagu 'Mardua Holong'.";
+
+const quotesParagraph = document.createElement("p");
+quotesParagraph.innerHTML =
+  "Jangan lupa makan <em>daging biang</em> dengan <em>sayur kol</em>";
+
+const lyricsHeading = document.createElement("h3");
+lyricsHeading.innerText = "Penggalan Lirik Mardua Holong";
+
+const lyricsDiv = document.createElement("div");
+lyricsDiv.innerHTML = `
+        <span>Tarsongon bunga naung malos di ladang i</span>
+        <br />
+        <span>Songoni ma rohakki nunga malala</span>
+        <br />
+        <span>Dang hurimpu songoni</span>
+        <br />
+        <span>Dibahen ho holonghi gabe meam-meammu</span>
+`;
+
+content.appendChild(welcomeHeading);
+content.appendChild(introParagraph);
+content.appendChild(quotesParagraph);
+content.appendChild(lyricsHeading);
+content.appendChild(lyricsDiv);
